8d7c16766d3deccea4ac78e44dc5e3d7b5a40a97
Luis Reche <rsnoverwatch@gmail.com>
2026-07-21 08:19:56 +0200

    release FragForge Studio 2.2.11

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -23,8 +23,8 @@ import HeroForge from "@/components/hero-forge";
 import Reveal from "@/components/reveal";
 
 const DOWNLOAD_URL =
-  "https://github.com/rechedev9/fragforge/releases/download/v2.2.10/FragForge.Studio.Setup.2.2.10.exe";
-const RELEASE_VERSION = "v2.2.10";
+  "https://github.com/rechedev9/fragforge/releases/download/v2.2.11/FragForge.Studio.Setup.2.2.11.exe";
+const RELEASE_VERSION = "v2.2.11";
 const REPO_URL = "https://github.com/rechedev9/fragforge";
 
 function Corners() {
